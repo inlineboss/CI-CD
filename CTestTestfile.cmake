7b156cb3743f7bb8fe6afaa5a09f547534fa0e88
Maxim <askimarhon@mail.ru>
2019-06-28 17:37:52 +0300

    start commit

---
+++ CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/facs/Work/homeproject/OTUS/otus_ci-cd
+# Build directory: /home/facs/Work/homeproject/OTUS/otus_ci-cd
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_version "test_version")
