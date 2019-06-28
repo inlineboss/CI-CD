ce89e910ea3cc8e5830716e7c6ae701ae13db132
FACS <askimarhond@gmail.com>
2019-06-28 23:33:24 +0300

    remove trash

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/facs/work/otus_ci-cd
-# Build directory: /home/facs/work/otus_ci-cd
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_version "test_version")
-add_test(gtest_version "gtest_version")
